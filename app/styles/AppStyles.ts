8c0da7e187e0114569150442a7ab1e0fc2372b04
ArghaPutra <alifarghaptra@gmail.com>
2026-02-27 23:50:14 +0700

    first commit

---
+++ app/styles/AppStyles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    paddingTop: 10,
+  },
+  card: {
+    width: 325,
+    marginVertical: 6,
+    borderRadius: 10,
+    elevation: 3,
+  },
+});
+
+export default styles;
